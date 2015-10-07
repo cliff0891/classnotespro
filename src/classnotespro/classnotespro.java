b2b921eadbbd3f9a1b977046c282bf82126f114b
cliff0891 <yuyunc@student.swosu.edu>
2015-10-07 10:19:06 -0500

    first commit

---
+++ src/classnotespro/classnotespro.java
@@ -0,0 +1,12 @@
+package classnotespro;
+
+public class classnotespro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Class!");
+
+	}
+
+}
